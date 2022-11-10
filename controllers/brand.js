a2ba1c0a11326d053e4e2e9309e0d703071e99e8
RIBKARANIANNAM <aribkarani2022@gmail.com>
2022-11-09 18:13:57 -0600

    updated

--- controllers/brand.js
+++ controllers/brand.js
@@ -19,8 +19,26 @@ exports.brand_detail = function(req, res) {
 }; 
  
 // Handle Costume create on POST. 
-exports.brand_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: brand create POST'); 
+exports.brand_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Brand(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+   // {"BrandName":"MK",  "Slogan":"Excellent", "Price":500}
+
+    document.BrandName = req.body.BrandName; 
+    document.Slogan = req.body.Slogan; 
+    document.Price = req.body.Price; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume delete form on DELETE. 
@@ -31,4 +49,17 @@ exports.brand_delete = function(req, res) {
 // Handle Costume update form on PUT. 
 exports.brand_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: brand update PUT' + req.params.id); 
+}; 
+
+// VIEWS 
+// Handle a show all view 
+exports.brand_view_all_Page = async function(req, res) { 
+    try{ 
+        thebrands = await Brand.find(); 
+        res.render('brand', { title: 'brand Search Results', results: thebrands }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
--- routes/brand.js
+++ routes/brand.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const brand_controlers= require('../controllers/brand'); 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('brand', { title: 'Search Results  Brands' });
-});
+router.get('/', brand_controlers.brand_view_all_Page ); 
 
 module.exports = router;
