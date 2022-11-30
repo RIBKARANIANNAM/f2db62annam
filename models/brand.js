058b14fbffd30bd931e653ee5ba811dc78ac0262
RIBKARANIANNAM <aribkarani2022@gmail.com>
2022-11-30 17:53:31 -0600

    changes updated

--- models/brand.js
+++ models/brand.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose") 
 const brandSchema = mongoose.Schema({ 
-    BrandName: String, 
-    Slogan: String, 
-    Price: Number 
+    BrandName: {
+        type: String,
+        required: [true, 'Brand name cannot be an empty String']
+    }, 
+    Slogan: {
+        type: String,
+        required: [true, "Slogan should be of tyoe String"]
+    },
+    Price: {
+        type: Number,
+        min: [ 1, 'Price should be of a number'],
+        max: 30
+    }
 }) 
  
 module.exports = mongoose.model("brand", brandSchema) 
